ffabe2e969927e600ccac90711f6fb88a557e6c0
Yasin Ramazan GOK <yasinrmzngok@gmail.com>
2025-01-02 03:59:08 +0300

    Section 24 has been completed.

---
+++ MYYouTubeBootCamp601/MYYouTubeBootCamp601/Services/CustomerOperations.cs
@@ -0,0 +1,41 @@
+﻿using MongoDB.Bson;
+using MYYouTubeBootCamp601.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MYYouTubeBootCamp601.Services
+{
+    public class CustomerOperations
+    {
+        public void AddCustomer(Customer customer)
+        {
+            var connection = new DatabaseConnectionString();
+            var customerCollection = connection.GetCustomersCollection();
+
+            var document = new BsonDocument
+            {
+                { "Name", customer.Name },
+                { "City", customer.City },
+                { "Balance", customer.Balance },
+                { "ShoppingCount", customer.ShoppingCount }
+            };
+
+            customerCollection.InsertOne(document);
+        }
+        public void UpdateCustomer()
+        {
+            Console.WriteLine("Customer Updated");
+        }
+        public void DeleteCustomer()
+        {
+            Console.WriteLine("Customer Deleted");
+        }
+        public void GetCustomer()
+        {
+            Console.WriteLine("Customer Retrieved");
+        }
+    }
+}
---
+++ MYYouTubeBootCamp601/MYYouTubeBootCamp601/Services/DatabaseConnectionString.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Bson; // for BsonDocument
+using MongoDB.Driver;
+using MYYouTubeBootCamp601.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MYYouTubeBootCamp601.Services
+{
+    public class DatabaseConnectionString
+    {
+        private IMongoDatabase _mongoDatabase;
+
+        public DatabaseConnectionString()
+        {
+            var client = new MongoClient("mongodb://localhost:27017");
+            _mongoDatabase = client.GetDatabase("CSharpBootCamp601Database");
+        }
+
+        public IMongoCollection<BsonDocument> GetCustomersCollection()
+        {
+            return _mongoDatabase.GetCollection<BsonDocument>("Customers");
+        }
+    }
+}
